7c5d551e74971e811d39e8bb910aee3ca37c80fd
Viktor Speranskyy <vencedor@ukr.net>
2019-08-01 17:50:16 +0300

    v.1

--- Threads5/Program.cs
+++ Threads5/Program.cs
@@ -1,15 +1,72 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Threads5
 {
-    class Program
+    class ThreadsRunners
     {
+        public static TextPresentation tp = new TextPresentation();
+        public static void Runner1()
+        {
+            Console.WriteLine("thread_1 run!");
+            Console.WriteLine("thread_1 – calling TextPresentation.showText");
+            tp.ShowText("*");
+            Console.WriteLine("thread_1 stop!");
+        }
+
+        public static void Runner2()
+        {
+            Console.WriteLine("thread_2 run!");
+            Console.WriteLine("thread_2 – calling TextPresentation.showText");
+            tp.ShowText("|");
+            Console.WriteLine("thread_2 stop!");
+        }
+
         static void Main(string[] args)
         {
+            ThreadStart runner1 = new ThreadStart(Runner1);
+            ThreadStart runner2 = new ThreadStart(Runner2);
+
+            Thread th1 = new Thread(runner1);
+            Thread th2 = new Thread(runner2);
+
+            th1.Start();
+            th2.Start();
+        }
+    }
+
+    class TextPresentation
+    {
+        public Mutex mutex;
+
+        public TextPresentation()
+        {
+            mutex = new Mutex();
+        }
+
+        public void ShowText(string text)
+        {
+            int i;
+            // Объект синхронизации в данном конкретном случае – представитель класса TextPresentation.
+            // Для его обозначения используется первичное выражение this.
+            //1. Блокировка кода монитором (начало) 
+            Monitor.Enter(this);
+            //2. Критическая секция кода (начало)   
+            lock (this)
+            {
+                mutex.WaitOne();//3.Блокировка кода мьютексом (начало)//
+                Console.WriteLine("\n" + (char)31 + (char)31 + (char)31 + (char)31);
+                for (i = 0; i < 360; i++)
+                {
+                    Console.Write(text);
+                }
+                Console.WriteLine("\n" + (char)30 + (char)30 + (char)30 + (char)30);
+                mutex.ReleaseMutex();
+                //3.Блокировка кода мьютексом (конец)//
+                //2. Критическая секция кода (конец) // 
+            }
+            //1. Блокировка кода монитором (конец) 
+            Monitor.Exit(this);
         }
     }
 }
